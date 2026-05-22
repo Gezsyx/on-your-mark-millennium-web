9a82abaab4a79e2fe6524778af214ae1e35250e1
Gezsyx <fajarabdulaziz259@gmail.com>
2026-05-23 04:34:31 +0700

    refactor: clean up code formatting and update response message in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,20 @@
-import "dotenv/config";
-import express from "express";
-import cors from "cors";
-import eventRoutes from "./routes/eventRoutes.js";
-import categoryRoutes from "./routes/categoryRoutes.js";
-import pembicaraRoutes from "./routes/pembicaraRoutes.js";
+import express from "express"
+import cors from "cors"
+import eventRoutes from "./routes/eventRoutes.js"
+import categoryRoutes from "./routes/categoryRoutes.js"
+import pembicaraRoutes from "./routes/pembicaraRoutes.js"
 
-const app = express();
-const port = 3000;
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+  res.send("API OK")
+})
 
-app.use("/events", eventRoutes);
-app.use("/pembicara", pembicaraRoutes);
-app.use("/categories", categoryRoutes);
+app.use("/events", eventRoutes)
+app.use("/categories", categoryRoutes)
+app.use("/pembicara", pembicaraRoutes)
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+export default app
